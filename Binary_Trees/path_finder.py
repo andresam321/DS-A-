545d881f40a9c6e1fd56620d3981bab6aaaf8758
Andres Mercado <andresmercado96@gmail.com>
2024-08-02 15:21:06 -0700

    path finder prob

---
+++ Binary_Trees/path_finder.py
@@ -0,0 +1,109 @@
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+
+
+
+def path_finder(root, target):
+  result = _path_finder(root, target)
+  if result is None:
+    return None
+  else:
+    return result[::-1]
+
+def _path_finder(root, target):
+  if root is None:
+    return None
+  
+  if root.val == target:
+    return [ root.val ]
+  
+  left_path = _path_finder(root.left, target)
+  if left_path is not None:
+    left_path.append(root.val)
+    return left_path
+  
+  right_path = _path_finder(root.right, target)
+  if right_path is not None:
+    right_path.append(root.val)
+    return right_path
+  
+  return None
+
+
+### test cases ###
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+path_finder(a, 'e') # -> [ 'a', 'b', 'e' ]
+
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+path_finder(a, 'p') # -> None
+
+
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+g = Node("g")
+h = Node("h")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+e.left = g
+f.right = h
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+#    /       \
+#   g         h
+
+path_finder(a, "c") # -> ['a', 'c']
