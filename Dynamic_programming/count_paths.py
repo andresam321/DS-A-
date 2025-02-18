de8b09d799dbef1928af338805cb44f64993e79f
andresam321 <andresmercado96@gmail.com>
2025-02-17 22:34:41 -0800

    count_paths

---
+++ Dynamic_programming/count_paths.py
@@ -0,0 +1,85 @@
+def count_paths(grid):
+  return _count_paths(grid, 0, 0, {})
+
+def _count_paths(grid, r, c, memo):
+  pos = (r, c)
+  if pos in memo:
+    return memo[pos]
+  
+  if r == len(grid) or c == len(grid[0]) or grid[r][c] == 'X':
+    return 0
+  
+  if r == len(grid) - 1 and c == len(grid[0]) - 1:
+    return 1
+  
+  memo[pos] = _count_paths(grid, r + 1, c, memo) + _count_paths(grid, r, c + 1, memo)
+  return memo[pos]
+
+grid = [
+  ["O", "O"],
+  ["O", "O"],
+]
+count_paths(grid) # -> 2
+
+grid = [
+  ["O", "O", "X"],
+  ["O", "O", "O"],
+  ["O", "O", "O"],
+]
+count_paths(grid) # -> 5
+
+grid = [
+  ["O", "O", "O"],
+  ["O", "O", "X"],
+  ["O", "O", "O"],
+]
+count_paths(grid) # -> 3
+
+
+grid = [
+  ["O", "O", "X", "O", "O", "O"],
+  ["O", "O", "X", "O", "O", "O"],
+  ["X", "O", "X", "O", "O", "O"],
+  ["X", "X", "X", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O"],
+]
+count_paths(grid) # -> 0
+
+
+grid = [
+  ["O", "O", "X", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "X"],
+  ["X", "O", "O", "O", "O", "O"],
+  ["X", "X", "X", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O"],
+]
+count_paths(grid) # -> 42
+
+grid = [
+  ["O", "O", "X", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "X"],
+  ["X", "O", "O", "O", "O", "O"],
+  ["X", "X", "X", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "X"],
+]
+count_paths(grid) # -> 0
+
+
+grid = [
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
+]
+count_paths(grid) # -> 40116600
