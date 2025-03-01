7a87cd30aef7363056fc7579b700f6314c72f353
andresam321 <andresmercado96@gmail.com>
2025-02-28 23:54:02 -0800

    refactor can_conc

--- Dynamic_programming/can_concat.py
+++ Dynamic_programming/can_concat.py
@@ -1,21 +1,44 @@
-def can_concat(s, words):
-  return _can_concat(s, words, {})
+# def can_concat(s, words):
+#   return _can_concat(s, words, {})
 
-def _can_concat(s, words, memo):
-  if s in memo:
-    return memo[s]
+# def _can_concat(s, words, memo):
+#   if s in memo:
+#     return memo[s]
   
-  if s == '':
-    return True
+#   if s == '':
+#     return True
   
-  for word in words:
-    if s.startswith(word):
-      suffix = s[len(word):]
-      if _can_concat(suffix, words, memo) == True:
-        memo[s] = True
-        return True
+#   for word in words:
+#     if s.startswith(word):
+#       suffix = s[len(word):]
+#       if _can_concat(suffix, words, memo) == True:
+#         memo[s] = True
+#         return True
       
-  memo[s] = False
+#   memo[s] = False
+#   return False
+
+
+
+def can_concat(s, words):
+  return _can_concat(s, words,0,{})
+
+def _can_concat(s, words,i, memo):
+  if i in memo:
+    return memo[i]
+  if i == len(s):
+    return True
+    
+  for word in words:
+    # print(word)
+#  _can_concat("pineapple", ["pine", "pinea", "apple", "pple"], i=4, memo={})
+  
+    if s.startswith(word,i):
+#                  pineapple
+      if _can_concat(s, words, i + len(word), memo):
+        memo[i] = True
+        return True
+  memo[i] = False
   return False
 can_concat("oneisnone", ["one", "none", "is"]) #  -> True
 can_concat("oneisnone", ["on", "e", "is"]) #  -> False
