6389bfb54da1e83374c2608b32636c1f8a1cef98
andresam321 <andresmercado96@gmail.com>
2025-03-04 22:37:32 -0800

    befitting_brackets solved

---
+++ Stacks/befitting_brackets.py
@@ -0,0 +1,23 @@
+def befitting_brackets(string):
+  stack = []
+  
+  brackets = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+  }
+  
+  for char in string:
+    if char in brackets:
+      stack.append(brackets[char])
+    else:
+      if stack and stack[-1] == char:
+        stack.pop()
+      else:
+        return False
+      
+  return len(stack) == 0
+
+befitting_brackets('(){}[](())') # -> True
+befitting_brackets('(){}[(()') # -> False
+befitting_brackets('(){}[()]') # -> True
