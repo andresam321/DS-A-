21f046fc326b984dc880a75c86adb5e45da87da8
andresam321 <andresmercado96@gmail.com>
2025-03-11 14:26:54 -0700

    permutations

---
+++ Exhaustive_Recursion/permutations.py
@@ -0,0 +1,42 @@
+def permutations(items):
+  if not items:
+    return [[]]
+  
+  first = items[0]
+  remaining = items[1:]
+  perms = permutations(remaining)
+  result = []
+  for perm in perms:
+    for i in range(len(perm) + 1):
+      result.append([*perm[:i], first, *perm[i:]])
+    
+  return result
+permutations(['a', 'b', 'c']) # -> 
+# [ 
+#   [ 'a', 'b', 'c' ], 
+#   [ 'b', 'a', 'c' ], 
+#   [ 'b', 'c', 'a' ], 
+#   [ 'a', 'c', 'b' ], 
+#   [ 'c', 'a', 'b' ], 
+#   [ 'c', 'b', 'a' ] 
+# ] 
+permutations(['red', 'blue']) # ->
+# [ 
+#   [ 'red', 'blue' ], 
+#   [ 'blue', 'red' ] 
+# ]
+permutations([8, 2, 1, 4]) # ->
+# [ 
+#   [ 8, 2, 1, 4 ], [ 2, 8, 1, 4 ], 
+#   [ 2, 1, 8, 4 ], [ 2, 1, 4, 8 ], 
+#   [ 8, 1, 2, 4 ], [ 1, 8, 2, 4 ], 
+#   [ 1, 2, 8, 4 ], [ 1, 2, 4, 8 ], 
+#   [ 8, 1, 4, 2 ], [ 1, 8, 4, 2 ], 
+#   [ 1, 4, 8, 2 ], [ 1, 4, 2, 8 ], 
+#   [ 8, 2, 4, 1 ], [ 2, 8, 4, 1 ], 
+#   [ 2, 4, 8, 1 ], [ 2, 4, 1, 8 ], 
+#   [ 8, 4, 2, 1 ], [ 4, 8, 2, 1 ], 
+#   [ 4, 2, 8, 1 ], [ 4, 2, 1, 8 ], 
+#   [ 8, 4, 1, 2 ], [ 4, 8, 1, 2 ], 
+#   [ 4, 1, 8, 2 ], [ 4, 1, 2, 8 ] 
+# ] 
